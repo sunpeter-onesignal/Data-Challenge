ec4f523e67d6047694dc237662b5a1d62c866da5
sunpeter-onesignal <peter@onesignal.com>
2022-08-23 13:51:38 -0700

    Add starter code

---
+++ src/average_degree.py
@@ -0,0 +1,21 @@
+import sys
+            
+
+def main(inputFile, outputFile):
+    """
+        This program will run through each tweet in the
+        inputFile and calculate the average degree of
+        hashtag. 
+        
+        What you have to do is figure out how to read the tweets from the input files,
+        calculate the average degree of each hashtag and write the output to the outputFile.
+    """
+
+    print('Input file is: ', inputFile)
+    print('Output file is: ', outputFile)
+
+    # Write 
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
